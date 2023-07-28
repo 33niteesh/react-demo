e81cb8c79389db67fe25bffc72e890461a1a6f4a
SATYAPU NITEESH KUMAR <82573249+33niteesh@users.noreply.github.com>
2023-07-28 22:26:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br/><br/>
+        <h3
+          className="App-link"
+        >
+          AWS BitBucket jira
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
